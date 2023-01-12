2ddbfcc85d62ae9b94cc60dba5e110780d8f8380
azurelmao <de_arcane@protonmail.com>
2023-01-12 17:01:18 +0100

    Add friction grace period

--- src/main/java/turniplabs/bhopboots/mixin/EntityLivingMixin.java
+++ src/main/java/turniplabs/bhopboots/mixin/EntityLivingMixin.java
@@ -3,23 +3,96 @@ package turniplabs.bhopboots.mixin;
 import net.minecraft.src.EntityLiving;
 import net.minecraft.src.EntityPlayer;
 import net.minecraft.src.ItemStack;
+import net.minecraft.src.helper.Utils;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import turniplabs.bhopboots.BhopBoots;
 
 @Mixin(value = EntityLiving.class, remap = false)
+@Debug(export = true)
 public class EntityLivingMixin {
     @Shadow
     protected float moveStrafing;
     @Unique
     private float oldYaw = 0.0F;
 
+    @Unique
+    private boolean lastTickOnGround;
+
+    @Unique
+    private boolean lastlastTickOnGround;
+
+    @ModifyVariable(method = "moveEntityWithHeading",
+            at = @At(value = "STORE", ordinal = 0),
+            slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/src/MathHelper;floor_double(D)I")),
+            ordinal = 2
+    )
+    private float bhopboots_moveEntityWithHeading1(float friction) {
+        EntityLiving entity = (EntityLiving) (Object) this;
+        if (entity instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) entity;
+
+            if (player.noClip) {
+                return friction;
+            }
+
+            ItemStack bootItem = player.inventory.armorItemInSlot(0);
+            if (bootItem == null) {
+                return friction;
+            }
+
+            if (bootItem.itemID != BhopBoots.bhopBoots.itemID) {
+                return friction;
+            }
+
+            if (!this.lastlastTickOnGround && this.lastTickOnGround) {
+                return 0.8f * 0.91f;
+            }
+        }
+
+        return friction;
+    }
+
+    @ModifyVariable(method = "moveEntityWithHeading",
+            at = @At(value = "STORE", ordinal = 0),
+            slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/src/MathHelper;floor_double(D)I", ordinal = 3)),
+            ordinal = 2
+    )
+    private float bhopboots_moveEntityWithHeading2(float friction) {
+        EntityLiving entity = (EntityLiving) (Object) this;
+        if (entity instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) entity;
+
+            if (player.noClip) {
+                return friction;
+            }
+
+            ItemStack bootItem = player.inventory.armorItemInSlot(0);
+            if (bootItem == null) {
+                return friction;
+            }
+
+            if (bootItem.itemID != BhopBoots.bhopBoots.itemID) {
+                return friction;
+            }
+
+            if (!this.lastlastTickOnGround && this.lastTickOnGround) {
+                return 0.8f * 0.91f;
+            }
+        }
+
+        return friction;
+    }
+
     @Inject(method = "onLivingUpdate", at = @At("HEAD"))
-    private void bunnyHop(CallbackInfo ci) {
+    private void bhopboots_onLivingUpdate1(CallbackInfo ci) {
         EntityLiving entity = (EntityLiving) (Object) this;
         if (entity instanceof EntityPlayer) {
             EntityPlayer player = (EntityPlayer) entity;
@@ -48,9 +121,20 @@ public class EntityLivingMixin {
             player.motionX = newX;
             player.motionZ = newZ;
 
-            float speed = 1.0f + Math.abs(this.moveStrafing) / 8.0f;
-            player.motionX *= speed;
-            player.motionZ *= speed;
+            float motion = 1.0f + Math.abs(this.moveStrafing) / 8.0f;
+            player.motionX *= motion;
+            player.motionZ *= motion;
         }
+    }
+
+    @Inject(method = "onLivingUpdate", at = @At("TAIL"))
+    private void bhopboots_onLivingUpdate2(CallbackInfo ci) {
+        EntityLiving entity = (EntityLiving) (Object) this;
+        if (entity instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) entity;
+            this.lastlastTickOnGround = this.lastTickOnGround;
+            this.lastTickOnGround = player.onGround;
+        }
+
     }
 }
